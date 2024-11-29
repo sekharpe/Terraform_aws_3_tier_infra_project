7b3fd06ace017dfa1f876d7d17726ac72e9e08d1
sekharpe <psekhar93440@gmail.com>
2024-11-29 16:32:30 +0530

    add new files

--- provider.tf
+++ provider.tf
@@ -17,6 +17,7 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
